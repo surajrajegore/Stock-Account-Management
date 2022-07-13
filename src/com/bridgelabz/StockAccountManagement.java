722b92fced122e36d11eac5d7e53b228ce134903
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-13 19:33:34 +0530

    Added Welcome message on master

---
+++ src/com/bridgelabz/StockAccountManagement.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class StockAccountManagement {
+    public static void main(String[] args){
+
+        System.out.println("Welcome to stock management program");
+
+       
+    }
+}
